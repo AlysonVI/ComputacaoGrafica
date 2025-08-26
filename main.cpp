c1442da7d60956c48bcef1b41481ef8707ad3703
AtosAiresArruda <atos.a.arruda@gmail.com>
2025-08-26 19:00:55 -0300

    Finalizado Objeto->Linha->Ponto

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include <ponto.h>
+#include <objeto.h>
 #include <mainwindowalyson.h>
 #include <QApplication>
 #include <iostream>
+
+using namespace std;
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     Ponto p1(1,2);
+    Ponto p2(3,4);
+    Ponto p3(10,2);
+    Ponto p4(3,43);
 
-    p1.mostraPonto();
+    Linha l1(p1,p2);
+    Linha l2(p3,p4);
+
+    Objeto o1("Reta");
+
+    o1.adicionaLinha(l1);
+    o1.adicionaLinha(l2);
+
+
+    o1.mostraLinhas();
 
     MainWindowAlyson w;
     w.show();
--- objeto.cpp
+++ objeto.cpp
@@ -1,5 +1,27 @@
 #include "objeto.h"
+#include <iostream>
 
-Objeto::Objeto()
-{
-}
+    Objeto::Objeto(){}
+    Objeto::Objeto(string nome){
+
+        this->nome = nome;
+        numLinhas = 0;
+    }
+
+    Objeto::~Objeto(){}
+
+    void Objeto::adicionaLinha(Linha l){
+        vetorDeLinhas.push_back(l);
+        numLinhas +=1;
+    }
+
+    void Objeto::mostraLinhas(){
+        for(int i =0; i<numLinhas; i++){
+            Linha laux = vetorDeLinhas[i];
+            cout << "Linha[" << i << "] (x:" << laux.a.x
+                 << " y:" << laux.a.y << ") + " << "(x:"
+                 << laux.b.x <<" y:"<< laux.b.y <<")";
+        }
+    }
+
+
